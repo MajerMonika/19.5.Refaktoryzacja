39bd9718acb00a39afa0b0e1c85ef3c70d8158fb
Monika Majer <majer_monika@interia.pl>
2018-12-17 22:17:05 +0100

    Change js files

--- components/App.js
+++ components/App.js
@@ -12,33 +12,47 @@ App = React.createClass({
     this.setState({
       loading: true
     });
-    this.getGif(searchingText, function (gif) {
-      this.setState({
-        loading: false,
-        gif: gif,
-        searchingText: searchingText
-      });
-    }.bind(this));
-  },
+    this.getGif(searchingText)
+      .then(gif => {
+		this.setState({
+		  loading: false,
+		  gif: gif,
+          searchingText: searchingText
+		});
+    })
+    .catch(error => console.error('Error!', error));
+},
 
-  getGif: function (searchingText, callback) {
+
+	getGif: function (searchingText) {
+		return new Promise( function (resolve, reject) {
     var GIPHY_API_URL = 'https://api.giphy.com';
     var GIPHY_PUB_KEY = 'iCBFMtDvMeou0fLvbK7HMhvGx9nFaAMo';
     var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function () {
+          
+    xhr.onload = function() {
+	 var data = JSON.parse(xhr.responseText).data;
+	 var gif = {
+       url: data.fixed_width_downsampled_url,
+       sourceUrl: data.url
+     };
+				
       if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText).data;
-        var gif = {
-          url: data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        callback(gif);
+        resolve(gif);
+      } else {
+        reject(new Error(this.statusText));
       }
     };
-    xhr.send();
-  },
+    
+    xhr.onerror = function() {
+      reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+    };
+
+    xhr.open('GET', url);
+	xhr.send();
+  });
+},
 
   render: function () {
 
@@ -50,8 +64,8 @@ App = React.createClass({
 
     return ( 
       <div style = {styles}>
-      <h1 > Wyszukiwarka GIFow! < /h1> <
-      p > Znajdź gifa na < a href = 'http://giphy.com' > giphy < /a>. Naciskaj enter, aby pobrać kolejne gify.</p >
+      <h1 > Wyszukiwarka GIFow! </h1> 
+      <p> Znajdź gifa na <a href = 'http://giphy.com'> giphy </a>. Naciskaj enter, aby pobrać kolejne gify.</p>
       <Search onSearch = {this.handleSearch}/> 
       <Gif 
         loading = {this.state.loading}
